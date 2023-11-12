07153f52759366a3c9b14dce8f3bc83b3494b0c5
Enthusiast101 <kaushal1anshul@gmail.com>
2023-11-12 20:30:42 +0530

    Commit 2.1

--- helpers/helper.py
+++ helpers/helper.py
@@ -1,6 +1,7 @@
+import numpy as np
 import tensorflow as tf
 
-class Metrics:
+class TfMetrics:
     def iou(self, true_mask, pred_mask) -> float:
         intersect = tf.reduce_sum(tf.multiply(pred_mask, true_mask))
         union = tf.reduce_sum(pred_mask) + tf.reduce_sum(true_mask) - intersect
@@ -49,6 +50,57 @@ class Metrics:
         TN = tf.reduce_sum(tf.cast(tf.equal(true_mask, pred_mask), tf.float32) - tf.reduce_sum(true_mask))
         FP = tf.reduce_sum(pred_mask) - TP
         FN = tf.reduce_sum(true_mask) - TP
+        num = 2 * (TP * TN - FN * FP)
+        den = (TP + FP) * (FP + TN) + (TP + FN) * (FN + TN)
+        return num/ den
+
+
+class NpMetrics:
+    def __init__(self, true_mask: np.ndarray, pred_mask: np.ndarray) -> None:
+        self.true_mask = true_mask
+        self.pred_mask = pred_mask
+
+    def iou(self,) -> float:
+        intersect = np.sum(self.pred_mask* self.true_mask)
+        union = np.sum(self.pred_mask) + np.sum(self.true_mask) - intersect
+        iou = intersect / union
+        return iou
+
+    def dsc(self,) -> float:
+        dsc_intersect = 2 * np.sum(self.pred_mask* self.true_mask)
+        dsc_union = 2 * np.sum(self.pred_mask) + np.sum(self.true_mask) - dsc_intersect
+        return dsc_intersect / dsc_union
+
+    def precision(self,) -> float:
+        intersect = np.sum(self.pred_mask* self.true_mask)
+        total_pixel_pred = np.sum(self.pred_mask)
+        return intersect / total_pixel_pred
+
+    def senstivity(self) -> float:
+        intersect = np.sum(self.pred_mask* self.true_mask)
+        total_pixel_truth = np.sum(self.true_mask)
+        return intersect / total_pixel_truth
+    
+    def specificity(self) -> float:  
+        intersect = np.sum(self.pred_mask* self.true_mask)
+        common = np.sum(self.true_mask == self.pred_mask)
+        total_pixel_pred = np.sum(self.pred_mask)
+        spec = (common - intersect) / (common + total_pixel_pred - 2 * intersect)
+        return spec
+
+    def randindex(self,) -> float:
+        intersect = np.sum(self.pred_mask* self.true_mask)
+        common = np.sum(self.true_mask == self.pred_mask)
+        total_pixel_pred = np.sum(self.pred_mask)
+        total_pixel_truth = np.sum(self.true_mask)
+        acc = common / (total_pixel_pred + total_pixel_truth + common - 2 * intersect)
+        return acc
+
+    def cohenkappa(self) -> float:
+        TP = np.sum(self.pred_mask* self.true_mask)
+        TN = np.sum(np.sum(self.true_mask == self.pred_mask) - np.sum(self.true_mask))
+        FP = np.sum(self.pred_mask) - TP
+        FN = np.sum(self.true_mask) - TP
         num = 2 * (TP * TN - FN * FP)
         den = (TP + FP) * (FP + TN) + (TP + FN) * (FN + TN)
         return num/ den
